61e2a8a1f11ea0e1ab40e4a2e0f7d9c130b00c0b
MikolajSulkowski <mikolajsulkowski675@gmail.com>
2024-12-18 16:15:00 +0100

    initial constructor

--- star/src/Star.java
+++ star/src/Star.java
@@ -1,4 +1,5 @@
 public class Star {
+
     private String name;
     private String cat_name;
     private float declination;
@@ -10,4 +11,19 @@ public class Star {
     private String hemisphere;
     private float temperature;
     private float mass;
+
+    public Star(String n, String cn, float dec, float rec, float obs, float abs, float lyd, String cons, String hemi, float temp, float ms) {
+         name = n;
+         cat_name = cn;
+         declination = dec;
+         rectascence = rec;
+         observ_size = obs;
+         absolute_size = abs;
+         lightYear_distance = lyd;
+         constellation = cons;
+         hemisphere = hemi;
+         temperature = temp;
+         mass = ms;
+    }
+
 }
