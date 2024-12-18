772987f462bf7316e32c87ea25b8d14aa2b2be5c
MikolajSulkowski <mikolajsulkowski675@gmail.com>
2024-12-18 15:44:33 +0100

    initial fields

---
+++ star/src/Star.java
@@ -0,0 +1,13 @@
+public class Star {
+    private String name;
+    private String cat_name;
+    private float declination;
+    private float rectascence;
+    private float observ_size;
+    private float absolute_size;
+    private float lightYear_distance;
+    private String constellation;
+    private String hemisphere;
+    private float temperature;
+    private float mass;
+}
