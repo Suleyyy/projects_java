ec2da09ce4c959fa63443f5ec2400b9fc6c4ac59
MikolajSulkowski <mikolajsulkowski675@gmail.com>
2024-12-27 11:55:10 +0100

    fields

---
+++ star/src/classes/Star.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Star {
+    private String name;
+    private String catName;
+    private int declination; //new class here
+    private int rightAscension; //new class here
+    private float apparentMagnitude;
+    private float absoluteMagnitude;
+    private float lightYears;
+    private String constellation; //new class here
+    private String hemisphere;
+    private float temperature;
+    private float mass;
+}
